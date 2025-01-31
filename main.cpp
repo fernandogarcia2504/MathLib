e50e18e7f6d81f57b2a1caa926de74d24a8354e2
fernando2504 <nando.garcia2504@gmail.com>
2025-01-30 18:17:10 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@ int main() {
     std::cout << "Sqrt(9) = " << my_sqrt(9) << '\n';
 
     // Pruebas de BasicOperations
-    std::cout << "2 + 3 = " << BasicOperations::add(2, 3) << '\n';
-    std::cout << "5 - 2 = " << BasicOperations::subtract(5, 2) << '\n';
-    std::cout << "4 * 6 = " << BasicOperations::multiply(4, 6) << '\n';
-    std::cout << "8 / 2 = " << BasicOperations::divide(8, 2) << '\n';
+    std::cout << "2 + 3 = " << BasicOperations::Add(2, 3) << '\n';
+    std::cout << "5 - 2 = " << BasicOperations::Subtract(5, 2) << '\n';
+    std::cout << "4 * 6 = " << BasicOperations::Multiply(4, 6) << '\n';
+    std::cout << "8 / 2 = " << BasicOperations::Divide(8, 2) << '\n';
 
     // Pruebas de AdvancedOperations
     std::cout << "Factorial(5) = " << AdvancedOperations::Factorial(5) << '\n';
