e639e9f7d0add3f4831da1b2882f21936db7cc49
fernando2504 <nando.garcia2504@gmail.com>
2025-01-30 18:30:48 -0600

    update

--- tests/AdvancedOperations_test.cpp
+++ tests/AdvancedOperations_test.cpp
@@ -1,21 +1,21 @@
 #include <gtest/gtest.h>
 #include "../AdvancedOperations.h"
 
-TEST(AdvancedOperationsTest, Factorial) {
-    EXPECT_EQ(120, AdvancedOperations::factorial(5));
-    EXPECT_EQ(1, AdvancedOperations::factorial(0));
+TEST(advanced_operations_test, Factorial) {
+    EXPECT_EQ(120, AdvancedOperations::Factorial(5));
+    EXPECT_EQ(1, AdvancedOperations::Factorial(0));
 }
 
-TEST(AdvancedOperationsTest, Combination) {
-    EXPECT_EQ(10, AdvancedOperations::combination(5, 3));
-    EXPECT_EQ(1, AdvancedOperations::combination(5, 5));
+TEST(advanced_operations_test, Combination) {
+    EXPECT_EQ(10, AdvancedOperations::Combination(5, 3));
+    EXPECT_EQ(1, AdvancedOperations::Combination(5, 5));
 }
 
-TEST(AdvancedOperationsTest, InvalidFactorial) {
-    EXPECT_THROW(AdvancedOperations::factorial(-1), std::runtime_error);
+TEST(advanced_operations_test, InvalidFactorial) {
+    EXPECT_THROW(AdvancedOperations::Factorial(-1), std::runtime_error);
 }
 
-TEST(AdvancedOperationsTest, InvalidCombination) {
-    EXPECT_THROW(AdvancedOperations::combination(-5, 3), std::runtime_error);
-    EXPECT_THROW(AdvancedOperations::combination(3, 5), std::runtime_error);
+TEST(advanced_operations_test, InvalidCombination) {
+    EXPECT_THROW(AdvancedOperations::Combination(-5, 3), std::runtime_error);
+    EXPECT_THROW(AdvancedOperations::Combination(3, 5), std::runtime_error);
 }
--- tests/BasicOperations_test.cpp
+++ tests/BasicOperations_test.cpp
@@ -2,21 +2,21 @@
 #include "../BasicOperations.h"
 
 TEST(basic_operations_test, Addition) {
-    EXPECT_EQ(5, BasicOperations::add(2, 3));
+    EXPECT_EQ(5, BasicOperations::Add(2, 3));
 }
 
 TEST(basic_operations_test, Subtraction) {
-    EXPECT_EQ(3, BasicOperations::subtract(5, 2));
+    EXPECT_EQ(3, BasicOperations::Subtract(5, 2));
 }
 
 TEST(basic_operations_test, Multiplication) {
-    EXPECT_EQ(24, BasicOperations::multiply(4, 6));
+    EXPECT_EQ(24, BasicOperations::Multiply(4, 6));
 }
 
 TEST(basic_operations_test, Division) {
-    EXPECT_EQ(4, BasicOperations::divide(8, 2));
+    EXPECT_EQ(4, BasicOperations::Divide(8, 2));
 }
 
 TEST(basic_operations_test, DivisionByZero) {
-    EXPECT_THROW(BasicOperations::divide(8, 0), std::runtime_error);
+    EXPECT_THROW(BasicOperations::Divide(8, 0), std::runtime_error);
 }
